892cf17fde51feae9bba95e4e91ea7d33f236a1f
Dave Dash <dd+github@davedash.com>
2011-06-17 14:08:50 -0700

    smarter staging update script

--- bin/update_staging.sh
+++ bin/update_staging.sh
@@ -17,28 +17,30 @@ $SVN up
 ./compile-mo.sh .
 popd > /dev/null
 
-
-
 # pull actual code
 $GIT pull -q origin master
 $GIT submodule update --init
 
 # pull vendor repo
 pushd vendor > /dev/null
+$GIT fetch origin
+NEWCODE=($GIT diff origin/master)
 $GIT pull -q origin master
 $GIT submodule update --init
 popd > /dev/null
 
-# Run database migrations.
-$PYTHON vendor/src/schematic/schematic migrations/
-$PYTHON vendor/src/schematic/schematic migrations/sites
+if [ $NEWCODE ]
+then
+        # Run database migrations.
+        $PYTHON vendor/src/schematic/schematic migrations/
+        $PYTHON vendor/src/schematic/schematic migrations/sites
 
-# Pull in highcharts.src.js - our lawyers make us do this.
-$PYTHON manage.py cron get_highcharts
-# Minify assets.
-$PYTHON manage.py compress_assets
+        # Pull in highcharts.src.js - our lawyers make us do this.
+        $PYTHON manage.py cron get_highcharts
+        # Minify assets.
+        $PYTHON manage.py compress_assets
+fi
 
 # Fix mobile and desktop site domains in database. Bug 608581.
 $PYTHON ./manage.py cron set_domains input.stage.mozilla.com m.input.stage.mozilla.com
-
 popd > /dev/null
